2b596a64ddb5010fa7b7694328ea48e1338e347c
Charalampos Kalligeros <charalampos.kalligeros@trasysgroup.com>
2019-12-22 21:57:56 +0200

    Fixing vulnerabilities

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { Credentials } from '../auth.interfaces';
 import { AuthService } from '../auth.service';
+import { StorageService } from '../storage.service';
 
 @Component({
 	selector: 'mw-login',
